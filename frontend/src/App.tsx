0f357745dd37e047e0ddb5029dfac5d7445c18c3
ilzat <ilzat.sadykov.2006@bk.ru>
2024-09-30 01:26:40 +0600

    'Сделал сам canvas'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,43 @@
 import './App.css'
+import {useEffect, useRef} from "react"
 
-const App = () => (
-    <>
-    </>
-);
+const App = () => {
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const context = canvas.getContext('2d');
+        if (!context) return;
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+
+        const draw = () => {
+            const canvas = canvasRef.current;
+            if (!canvas) return;
+            const context = canvas.getContext('2d');
+            if (!context) return;
+
+            canvas.addEventListener('mousedown', (e) => {
+                context.beginPath();
+                context.arc(e.clientX, e.clientY, 30, 0, Math.PI * 2);
+                context.fill();
+            })
+        }
+
+        draw()
+    }, [])
+
+
+    return (
+        <canvas ref={canvasRef}>
+            Canvas
+        </canvas>
+    )
+};
 
 export default App
