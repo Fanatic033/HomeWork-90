5d61277184391cb126643f8620ed961812e77029
ilzat <ilzat.sadykov.2006@bk.ru>
2024-09-30 21:16:07 +0600

    'Настроил сам Вебсокет сделал массив пикселей прохожусь по нему и вывожу

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,56 @@
 import express from "express";
+import cors from "cors";
+import expressWs from "express-ws";
+import {WebSocket} from "ws";
 
 
 const app = express();
+expressWs(app);
+
 const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+
+const router = express.Router();
+
+let connectedClients: WebSocket[] = []
+let pixels: WebSocket[] = [];
+
+router.ws('/canvas', async (ws, req) => {
+    connectedClients.push(ws)
+    ws.send(JSON.stringify({type: 'INIT', pixels: pixels}));
+
+    ws.on('message', (message) => {
+        try {
+            const decodedData = JSON.parse(message.toString());
+
+            if (decodedData.type === 'DRAW') {
+                pixels.push(...decodedData.pixels);
+
+                connectedClients.forEach((clientWS) => {
+                    clientWS.send(JSON.stringify({type: 'DRAW', pixels: decodedData.pixels}));
+                })
+            }
+
+
+        } catch (err) {
+            console.log(err);
+        }
+    })
+
+    ws.on('close', (req, res) => {
+        console.log('client disconnected');
+        connectedClients.splice(connectedClients.indexOf(ws), 1);
+    })
+
+})
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`server started on port: ${port}`);
+})
+
+
+
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,89 @@
-import './App.css'
-import {useEffect, useRef} from "react"
+import './App.css';
+import {useEffect, useRef} from 'react';
 
 const App = () => {
-
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const ws = useRef<WebSocket | null>(null);
+    const isDrawing = useRef<boolean>(false);
+    const drawingPixels = useRef<Array<{ x: number; y: number }>>([]);
 
     useEffect(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
+        ws.current = new WebSocket('ws://localhost:8000/canvas');
 
-        const context = canvas.getContext('2d');
-        if (!context) return;
+        ws.current.onclose = () => console.log('Connection closed');
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-
-
-        const draw = () => {
+        ws.current.onmessage = (event) => {
+            const data = JSON.parse(event.data);
             const canvas = canvasRef.current;
             if (!canvas) return;
             const context = canvas.getContext('2d');
             if (!context) return;
 
-            canvas.addEventListener('mousedown', (e) => {
-                context.beginPath();
-                context.arc(e.clientX, e.clientY, 30, 0, Math.PI * 2);
-                context.fill();
-            })
-        }
+            if (data.type === 'INIT') {
+                data.pixels.forEach((pixel: { x: number; y: number }) => {
+                    drawPixel(context, pixel.x, pixel.y);
+                });
+            }
+            if (data.type === 'DRAW') {
+                data.pixels.forEach((pixel: { x: number; y: number }) => {
+                    drawPixel(context, pixel.x, pixel.y);
+                });
+            }
+        };
 
-        draw()
-    }, [])
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
 
-    return (
-        <canvas ref={canvasRef}>
-            Canvas
-        </canvas>
-    )
+        const startDrawing = (e: MouseEvent) => {
+            isDrawing.current = true;
+            draw(e);
+        };
+
+
+        const stopDrawing = () => {
+            if (!isDrawing.current) return;
+            isDrawing.current = false;
+
+
+            if (ws.current) {
+                ws.current.send(JSON.stringify({type: 'DRAW', pixels: drawingPixels.current}));
+            }
+        };
+
+        const draw = (e: MouseEvent) => {
+            if (!isDrawing.current) return;
+            const canvas = canvasRef.current;
+            if (!canvas) return;
+            const context = canvas.getContext('2d');
+            if (!context) return;
+
+            const rect = canvas.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            drawPixel(context, x, y);
+
+            drawingPixels.current.push({x, y});
+        };
+
+        const drawPixel = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
+            ctx.beginPath();
+            ctx.fillStyle = 'black';
+            ctx.arc(x,y, 15, 0, Math.PI * 2);
+            ctx.fill()
+        };
+
+        canvas.addEventListener('mousedown', startDrawing);
+        canvas.addEventListener('mousemove', draw);
+        canvas.addEventListener('mouseup', stopDrawing);
+
+        return;
+    }, []);
+
+    return <canvas ref={canvasRef}>Canvas</canvas>;
 };
 
-export default App
+export default App;
